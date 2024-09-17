0bd7c36a2e481d62fa8cf629685dc5b531588dfb
ilyashe <ilya.shebanoff@yandex.ru>
2024-09-17 11:50:17 +0300

    Add homework from previous module

---
+++ lesson4_step6.py
@@ -0,0 +1,16 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+try:
+    browser = webdriver.Chrome()
+    # говорим WebDriver искать каждый элемент в течение 5 секунд
+    browser.implicitly_wait(5)
+
+    browser.get("http://suninjuly.github.io/cats.html")
+    browser.find_element(By.ID, "button")
+
+finally:
+    # ожидание чтобы визуально оценить результаты прохождения скрипта
+    time.sleep(3)
+    # закрываем браузер после всех манипуляций
+    browser.quit()
